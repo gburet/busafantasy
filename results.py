3ee02233743855a4a0e80a0bd39ead6b0c18d601
Guillaume Buret <hayato11@hotmail.fr>
2015-11-17 18:23:11 +0100

    Update results.py

--- results.py
+++ results.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import os
+import json
+import shutil
+import getpass
 import argparse
 import colorama
 import BeautifulSoup as bs
@@ -9,12 +11,17 @@ from requests import session
 
 ######### PLACE YOUR CONFIG HERE ##########
 
-RES_FILE    = "/home/gburet/.fantasy/res.txt"
+EMAIL       = "hayato11@hotmail.fr"
 PLAYER_NAME = "The Answer"
-EMAIL = ""
-PASSWORD = ""
+
+RES_FILE           = "/home/gburet/.fantasy/res.txt"
+BACKUP_RES_FILE    = "/home/gburet/.fantasy/backup_res.txt"
+
 
 ######### END OF CONFIG - START CODE ##########
+
+DATE_LABEL   = "Date"
+PLAYER_LABEL = "Players"
 
 LINE_LEN = 48
 
@@ -25,14 +32,7 @@ GREEN   = colorama.Fore.GREEN + colorama.Style.BRIGHT
 BACK_BLUE = colorama.Back.BLUE + colorama.Style.BRIGHT
 BACK_CYAN = colorama.Back.CYAN + colorama.Style.BRIGHT
 
-# Get last saved results
-with open(RES_FILE) as f:
-    lines = f.readlines()
-RESULTS = {r.split(':')[0] : int(r.split(':')[1].strip()) for r in lines if ':' in r}
 
-# Methods to sort players, get score difference, ...
-def sort_players(dico):
-    return sorted(dico, key=lambda x: dico[x], reverse=True)
 
 def diff_score(val1, val2):
     if val1 > val2:
@@ -40,104 +40,203 @@ def diff_score(val1, val2):
     else:
         return GREEN + " (+{})".format(val2-val1) + ENDC
 
+def enter_date():
+    print "Enter new date "
+    new_date = raw_input()
+    return new_date
 
-# Print players in a given order
-def print_players(players):
-    print BACK_BLUE + ' '*LINE_LEN + ENDC
-    for index, player in enumerate(players):
-        info = BACK_BLUE + ' ' + ENDC + ' '
-        res  = str(index+1) + ". " + player.ljust(16) + " : " + str(RESULTS[player])
-        if player == PLAYER_NAME:
-            res = BACK_CYAN + res + ENDC
+
+class Result(object):
+    
+    def __init__(self, player_name, player_result):
+        self.player_name = player_name
+        self.player_result = player_result
+
+    def get_player_name(self):
+        return self.player_name
+
+    def get_player_result(self):
+        return self.player_result
+
+
+class DailyResults(object):
+    
+    def __init__(self, results, date):
+        self.date = date
+        self.results = [Result(name, results[name]) for name in results]
+
+    def get_player_names(self):
+        return [r.get_player_name() for r in self.results]
+
+    def get_player_names_sorted(self):
+        sorted_results = sorted(self.results, key=lambda x: x.get_player_result(), reverse=True)
+        return [s.get_player_name() for s in sorted_results]
+
+    def get_results(self):
+        return {r.get_player_name() : r.get_player_result() for r in self.results}
+
+    def get_date(self):
+        return self.date
+
+    def get_score_for_player(self, player_name):
+        return self.get_results().get(player_name, None)
+
+
+class Results(object):
+    
+    DATE_LABEL   = "Date"
+    PLAYER_LABEL = "Players"
+    
+    def __init__(self):
+        self.all_daily_results = {}
+
+    @classmethod
+    def json_loader(cls, path=RES_FILE):
+        with open(path) as json_file:
+            data = json_file.read()
+        all_results = json.loads(data)
+        
+        inst = cls()
+        for result in all_results:
+            inst.all_daily_results[result] = DailyResults(all_results[result][cls.PLAYER_LABEL], 
+                                                          all_results[result][cls.DATE_LABEL])
+        
+        return inst
+
+    def get_player_names_sorted(self):
+        last_date_index = self.get_last_date_index()
+        return self.all_daily_results[last_date_index].get_player_names_sorted()
+
+    def get_last_date_index(self):
+        return str(max([int(s) for s in self.all_daily_results.keys()]))
+
+    # Print players in a given order
+    def print_results(self, date_index=None):
+        if date_index is None:
+            date_index = self.get_last_date_index()
+
+        print BACK_BLUE + ' '*LINE_LEN + ENDC
+        daily_results = self.all_daily_results[date_index]
+        for player_id, player_name in enumerate(daily_results.get_player_names_sorted()):
+            info = BACK_BLUE + ' ' + ENDC + ' '
+            res  = str(player_id+1) + ". " + player_name.ljust(16) + " : " + str(daily_results.get_score_for_player(player_name))
+            if player_name == PLAYER_NAME:
+                res = BACK_CYAN + res + ENDC
+            else:
+                res += diff_score(daily_results.get_score_for_player(player_name), daily_results.get_score_for_player(PLAYER_NAME)) 
+            info += res.ljust(LINE_LEN-2+2*6) + BACK_BLUE + ' ' + ENDC
+    
+            print info
+        print BACK_BLUE + ' '*LINE_LEN + ENDC
+    
+     
+    # Print players in a given order
+    def print_players_and_evolution(self, old_index):
+        current_results = self.all_daily_results[self.get_last_date_index()]
+        old_results     = self.all_daily_results[old_index]
+        
+        print BACK_BLUE + ' '*LINE_LEN + ENDC
+        for player_id, player_name in enumerate(current_results.get_player_names_sorted()):
+            info = BACK_BLUE + ' ' + ENDC + ' '
+            res  = str(player_id+1) + ". " + player_name.ljust(16) + " : " + str(current_results.get_score_for_player(player_name))
+            if player_name == PLAYER_NAME:
+                res = BACK_CYAN + res + ENDC + "      "
+            else:
+                res += diff_score(current_results.get_score_for_player(player_name), 
+                                  current_results.get_score_for_player(PLAYER_NAME))
+            res += "   (+{})".format(current_results.get_score_for_player(player_name) - old_results.get_score_for_player(player_name))
+            info += res.ljust(LINE_LEN-2+2*6) + BACK_BLUE + ' ' + ENDC
+     
+            print info
+        print BACK_BLUE + ' '*LINE_LEN + ENDC
+     
+     
+    # Enter new values
+    def enter_players(self):
+        new_date = enter_date()
+        
+        new_results = {}
+        for player in self.get_player_names_sorted():
+            print "Enter new score for {}".format(player)
+            score = int(raw_input())
+            new_results[player] = score
+        print ""
+
+        last_index = self.get_last_date_index()
+        
+        self.all_daily_results[str(int(last_index)+1)] = DailyResults(new_results, new_date)
+
+        self.print_players_and_evolution(last_index)
+        return new_results
+     
+     
+    def save_results(self):
+        print 'save results ? (y/n)'
+        ans = raw_input()
+        if ans == "y":
+            dico = {}
+            for key in self.all_daily_results:
+                dico[key] = {self.DATE_LABEL   : self.all_daily_results[key].get_date(),
+                             self.PLAYER_LABEL : self.all_daily_results[key].get_results()}
+           
+            shutil.copy(RES_FILE, BACKUP_RES_FILE)
+            with open(RES_FILE, 'w') as f:
+                f.write(json.dumps(dico))
+
+            print "results saved !"
         else:
-            res += diff_score(RESULTS[player], RESULTS[PLAYER_NAME])
-        info += res.ljust(LINE_LEN-2+2*6) + BACK_BLUE + ' ' + ENDC
+            print "results not saved"
+    
+    def get_results_from_busa(self):
+        """
+            Get teams, rankings, points from busa
+            Return a dict
+        """
+        password = getpass.getpass()
+        payload = {'logmod': '1',
+                   'FrmEma': EMAIL,
+                   'FrmPas': password}
+    
+        with session() as c_session:
+            c_session.post('http://fantasy.2ics.net/asp/mai_utilisateurs/log_mod.asp', data=payload)
+            response = c_session.get('http://fantasy.2ics.net/asp/mai_ligues/lig_fic.asp?ligid=4441')
+            c_classement_html = response.text
+    
+        # Parse html
+        data_html = bs.BeautifulSoup(c_classement_html)
+    
+        # Build rankings, points and team_name
+        rankings = [int(elt.text.split('(')[1].replace('e)', ''))
+                    for elt in data_html.findAll('span', attrs={'class': 'equipe-points'})]
+        points = [(int(elt.text.split(' ')[0])) for elt in data_html.findAll('span', attrs={'class': 'equipe-points'})]
+        team_name = [team.text.split('. ')[1] for team in data_html.findAll('span', attrs={'class': 'equipe-nom'})]
+    
+        # Build result dictionary
+        data = {team : point for team, point, _ in zip(team_name, points, rankings)}
+    
+        last_index = self.get_last_date_index()
+        self.all_daily_results[str(int(last_index)+1)] = DailyResults(data, None)
+        self.print_results()
+        
 
-        print info
-    print BACK_BLUE + ' '*LINE_LEN + ENDC
-
-# Print players in a given order
-def print_players_and_evolution(players, dico):
-    print BACK_BLUE + ' '*LINE_LEN + ENDC
-    for index, player in enumerate(players):
-        info = BACK_BLUE + ' ' + ENDC + ' '
-        res  = str(index+1) + ". " + player.ljust(16) + " : " + str(dico[player])
-        if player == PLAYER_NAME:
-            res = BACK_CYAN + res + ENDC
-        else:
-            res += diff_score(dico[player], dico[PLAYER_NAME])
-        res += "   (+{})".format(dico[player]-RESULTS[player])
-        info += res.ljust(LINE_LEN-2+2*6) + BACK_BLUE + ' ' + ENDC
-
-        print info
-    print BACK_BLUE + ' '*LINE_LEN + ENDC
-
-
-
-# Enter new values
-def enter_players():
-    new_results = {}
-    for player in sort_players(RESULTS):
-        print "Entrer le nouveau score de {}".format(player)
-        score = int(raw_input())
-        new_results[player] = score
-    print ""
-    print_players_and_evolution(sort_players(new_results), new_results)
-    return new_results
-
-
-def save_results(dico):
-    print 'save results ? (y/n)'
-    ans = raw_input()
-    if ans == "y":
-        with open(RES_FILE, 'w') as f:
-            f.write('\n'.join(["{}:{}".format(k,d) for k,d in dico.iteritems()]))
-        print "results saved !"
-    else:
-        print "results not saved"
-
-def get_results_from_busa():
-    """
-        Get teams, rankings, points from busa
-        Return a dict
-    """
-
-    payload = {'logmod': '1',
-               'FrmEma': EMAIL,
-               'FrmPas': PASSWORD}
-
-    with session() as c_session:
-        c_session.post('http://fantasy.2ics.net/asp/mai_utilisateurs/log_mod.asp', data=payload)
-        response = c_session.get('http://fantasy.2ics.net/asp/mai_ligues/lig_fic.asp?ligid=4441')
-        c_classement_html = response.text
-
-    # Parse html
-    data_html = bs.BeautifulSoup(c_classement_html)
-
-    # Build rankings, points and team_name
-    rankings = [int(elt.text.split('(')[1].replace('e)', ''))
-                for elt in data_html.findAll('span', attrs={'class': 'equipe-points'})]
-    points = [(int(elt.text.split(' ')[0])) for elt in data_html.findAll('span', attrs={'class': 'equipe-points'})]
-    team_name = [team.text.split('. ')[1] for team in data_html.findAll('span', attrs={'class': 'equipe-nom'})]
-
-    # Build result dictionary
-    data = {team: {'points': point, 'classement': classement} for team, point, classement in zip(team_name, points, rankings)}
-
-    return data
 
 if __name__ == "__main__":
-
-    PARSER = argparse.ArgumentParser(description='Suivi des points pour la Fantasy League')
-    PARSER.add_argument('-l', default='', action="store_true",
-                        help='Liste les joueurs')
-    PARSER.add_argument('-s', default='', action="store_true",
-                        help='Entre de nouveaux résultats')
+    
+    PARSER = argparse.ArgumentParser(description='Basket USA Fantasy League tool that helps points following')
+    PARSER.add_argument('-l', default='', action="store_true", help='List players')
+    PARSER.add_argument('-s', default='', action="store_true", help='Enter manually new results')
+    PARSER.add_argument('-d', default='', action="store_true", help='Get results from Basket USA')
     ARGS = PARSER.parse_args()
+ 
+    R=Results.json_loader()
 
     if ARGS.l != '':
-        print_players(sort_players(RESULTS))
+        R.print_results()
     elif ARGS.s != '':
-        NEW = enter_players()
-        save_results(NEW)
+        R.enter_players()
+        R.save_results()
+    elif ARGS.d != '':
+        R.get_results_from_busa()
     else:
         PARSER.print_help()
+
